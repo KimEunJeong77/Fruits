ee78eb27999d0afc71e976ffda3350cd3fbc5cf5
KimEunJeong77 <cheritis@naver.com>
2021-12-29 16:27:47 +0900

    Start project

---
+++ src/components/Fruits.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>과일목록</div>
+    <ul>
+        <li 
+            v-for="fruit in fruits"
+            :key="fruit" >
+            {{ fruit }}
+        </li>
+    </ul>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            fruits:['Apple','Banana','Cherry']
+        }
+    }
+}
+</script>
